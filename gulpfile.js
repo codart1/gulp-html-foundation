607993b6f1c0625bac9dde57c2f854ce5daade18
ManhDT <manhdt@trams.co.kr>
2015-08-27 16:24:12 +0700

    Add jade inheritance

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,11 @@ var connect = require('gulp-connect');
 var changed = require('gulp-changed');
 
 var watch = require('gulp-watch');
+var jadeInheritance = require('gulp-jade-inheritance');
+var cached = require('gulp-cached');
+var gulpif = require('gulp-if');
+
+var filter = require('gulp-filter');
 
 function swallowError (error) {
 
@@ -54,7 +59,7 @@ gulp.task('connect', function() {
 });
 
 // Watch
-gulp.task('watch', ['jade', 'less'], function() {
+gulp.task('watch', ['setWatch', 'jade', 'less'], function() {
   //gulp.watch("./src/jade/**/*.jade", ['jade']);
   //gulp.watch("./src/less/**/*.less", ['less']);
 
@@ -65,6 +70,40 @@ gulp.task('watch', ['jade', 'less'], function() {
 
 // Jade
 gulp.task('jade', function() {
+  var src = './src/jade/**/*.jade';
+
+  var dest = './dist/html';
+
+    gulp.src(src)
+
+        .pipe(changed(dest, {extension: '.html'}))
+
+        //filter out unchanged partials, but it only works when watching
+        .pipe(gulpif(global.isWatching, cached('jade')))
+
+        //find files that depend on the files that have changed
+        .pipe(jadeInheritance({basedir: './src/jade/'}))
+
+        //filter out partials 
+        .pipe(filter(function (file) {
+            return !/layouts/.test(file.path) && !/includes/.test(file.relative);
+        }))
+
+        .pipe(jade({
+            pretty: true
+        }))
+
+        .on('error', swallowError)
+        .pipe(gulp.dest(dest))
+});
+
+//Set watch
+gulp.task('setWatch', function() {
+    global.isWatching = true;
+});
+
+// Jade process all
+gulp.task('jade_process_all', function() {
   var src = [
     './src/jade/**/*.jade',
     '!./src/jade/**/layouts/*.jade',
@@ -74,7 +113,6 @@ gulp.task('jade', function() {
   var dest = './dist/html';
 
     gulp.src(src)
-        .pipe(changed(dest, {extension: '.html'}))
         .pipe(jade({
             pretty: true
         }))
@@ -102,5 +140,6 @@ gulp.task('less', function() {
 });
 
 
-gulp.task('default', ['jade', 'less', 'serve', 'watch']);
-gulp.task('ss', ['jade', 'less', 'connect', 'watch']);
+gulp.task('dist', ['jade_process_all', 'less']);
+gulp.task('default', ['jade_process_all', 'less', 'serve', 'watch']);
+gulp.task('ss', ['jade_process_all', 'less', 'connect', 'watch']);
